32f68d7d796f296473a50c0dec0be0fb465fec50
Ian Rich <ianrichb52@outlook.com>
2025-04-03 17:13:14 -0500

    Show how long our Data Saving takes in our message.

--- src/main/java/net/minebo/mcraidz/thread/DataSyncThread.java
+++ src/main/java/net/minebo/mcraidz/thread/DataSyncThread.java
@@ -1,11 +1,15 @@
 package net.minebo.mcraidz.thread;
 
+import com.google.common.base.Stopwatch;
 import net.minebo.mcraidz.MCRaidz;
 import net.minebo.mcraidz.profile.ProfileManager;
 import net.minebo.mcraidz.team.TeamManager;
+import org.apache.commons.lang3.time.StopWatch;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.concurrent.TimeUnit;
 
 public class DataSyncThread extends BukkitRunnable {
 
@@ -13,12 +17,19 @@ public class DataSyncThread extends BukkitRunnable {
     public void run() {
 
         // Only save online profiles.
+
+        Stopwatch stopwatch = Stopwatch.createUnstarted();
+
+        stopwatch.start();
+
         Bukkit.getOnlinePlayers().forEach(p -> {ProfileManager.saveProfile(ProfileManager.getProfileByPlayer(p));});
         TeamManager.teams.forEach(TeamManager::saveTeam);
 
+        stopwatch.stop();
+
         Bukkit.getOnlinePlayers().forEach(player -> {
             if(player.hasPermission("basic.admin")) {
-                player.sendMessage(ChatColor.YELLOW + "[DataSync] " + ChatColor.GRAY + "Saved " + ProfileManager.profiles.size() + " profiles and " + TeamManager.teams.size() + ChatColor.GRAY + " teams!");
+                player.sendMessage(ChatColor.YELLOW + "[DataSync] " + ChatColor.GRAY + "Saved " + Bukkit.getOnlinePlayers().size() + " profiles and " + TeamManager.teams.size() + ChatColor.GRAY + " teams!" + ChatColor.RESET + " (" + stopwatch.elapsed(TimeUnit.MILLISECONDS) + "ms)");
             }
         });
     }
