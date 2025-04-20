c6cbec6cab7e8ce517aea3abc663e523e40ce871
Ian Rich <ianrichb52@outlook.com>
2025-04-20 00:52:11 -0500

    Add a paginated /myshop command, we're on a roll tonight. HOT DAYUM

---
+++ src/main/java/net/minebo/mcraidz/shop/command/MyShopCommand.java
@@ -0,0 +1,55 @@
+package net.minebo.mcraidz.shop.command;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.*;
+import co.aikar.commands.annotation.Optional;
+import net.minebo.mcraidz.shop.ShopManager;
+import net.minebo.mcraidz.shop.construct.ShopItem;
+import net.minebo.mcraidz.util.ItemUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@CommandAlias("myshop")
+public class MyShopCommand extends BaseCommand {
+
+    private static final int ITEMS_PER_PAGE = 5;
+
+    @Default
+    @Syntax("[page]")
+    public void onMyShop(Player player, @Optional Integer pageOptional) {
+        UUID playerUUID = player.getUniqueId();
+
+        List<ShopItem> myItems = ShopManager.shopItems.stream()
+                .filter(item -> item.getOwner().equals(playerUUID))
+                .collect(Collectors.toList());
+
+        if (myItems.isEmpty()) {
+            player.sendMessage(ChatColor.GRAY + "You have no items listed in the market.");
+            return;
+        }
+
+        int totalPages = (int) Math.ceil(myItems.size() / (double) ITEMS_PER_PAGE);
+        int page = pageOptional != null ? Math.max(1, Math.min(pageOptional, totalPages)) : 1;
+
+        int start = (page - 1) * ITEMS_PER_PAGE;
+        int end = Math.min(start + ITEMS_PER_PAGE, myItems.size());
+
+        player.sendMessage(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "------------------------");
+        player.sendMessage(ChatColor.GOLD + "Your Shop Listings " + ChatColor.GRAY + "(Page " + page + "/" + totalPages + ")");
+        player.sendMessage("");
+
+        for (int i = start; i < end; i++) {
+            ShopItem item = myItems.get(i);
+            player.sendMessage(ChatColor.YELLOW + "- " + ChatColor.YELLOW + item.getName().toUpperCase() +
+                    ChatColor.GRAY + " | Price: " + ChatColor.GOLD + "⛃" + ChatColor.YELLOW + item.getPrice());
+        }
+
+        player.sendMessage("");
+        player.sendMessage(ChatColor.GRAY + "Use " + ChatColor.YELLOW + "/myshop <page>" + ChatColor.GRAY + " to view other pages.");
+        player.sendMessage(ChatColor.DARK_GRAY + "" + ChatColor.STRIKETHROUGH + "------------------------");
+    }
+}
